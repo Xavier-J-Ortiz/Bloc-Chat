646efd3dadb6250579b5b4a266a14a84904c4f71
xortiz <cuchumino@gmail.com>
2017-01-13 17:08:39 -0500

    added ng-model= in the html instead of id= in the input. With the ng-model parameter, I was able to use this in the ng-click function without any problems whatsoever. Now, I need to clean up some things visually, and also make the modal dissappear when I click to create a new room.

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -5,10 +5,8 @@
                   
     return {
         all: rooms,
-        roomCreate : (function(){
-            var newIndex = rooms.length + 1;
-            var roomName = "room" + newIndex;
-            rooms.$add(roomName).then(function(ref) {
+        roomCreate : (function(newName){
+            rooms.$add(newName).then(function(ref) {
                 var id = ref.key;
                 console.log("added record with id " + id);
                 rooms.$indexFor(id);
