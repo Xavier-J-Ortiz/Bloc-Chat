fce7e083fb5b13ee48dfce9a0eab661922da8d5d
xortiz <cuchumino@gmail.com>
2017-01-12 20:08:16 -0500

    added new room creation functionality via angularfire . I got hung up on the fact that the ID that  created was not the one I wanted, but then I realized that it didn't matter. Next commit will include uibModal that will allow custom room name info

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -2,20 +2,19 @@
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-      
-      var roomCreate = function(){
-          var newIndex = rooms.length + 1;
-          var roomName = "room" + newIndex;
-          rooms.$add({newIndex : roomName}).then(function(ref) {
-              var id = ref.key;
-              console.log("added record with id " + id);
-              list.$indexFor(id);
-          });
-      };
-      
+                  
     return {
-      all: rooms
-    };
+        all: rooms,
+        roomCreate : (function(){
+            var newIndex = rooms.length + 1;
+            var roomName = "room" + newIndex;
+            rooms.$add(roomName).then(function(ref) {
+                var id = ref.key;
+                console.log("added record with id " + id);
+                rooms.$indexFor(id);
+            });
+        })
+    };      
   }
 
   angular
