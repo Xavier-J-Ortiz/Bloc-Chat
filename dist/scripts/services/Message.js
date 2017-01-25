506e48c41af0536d4bfce933c7d66697c745cc01
xortiz <cuchumino@gmail.com>
2017-01-24 21:48:39 -0500

    started to set up message test box at the bottom. needs some aligning. Calling it a night however.

--- dist/scripts/services/Message.js
+++ dist/scripts/services/Message.js
@@ -3,24 +3,31 @@
       var ref = firebase.database().ref().child("messages");
       var messages = $firebaseArray(ref);
       
+      var getByRoomId = function(roomId){
+          var roomMessages = [];
+          if (roomId !== undefined){                  
+              ref.orderByChild("roomId").equalTo(roomId).on('value', function(snapshot) {
+                  snapshot.val().forEach(function(element){
+                      if (element != null){
+                          roomMessages.push(element);
+                      }
+                  });
+              });                  
+          }
+          return roomMessages;
+      };
+      
+      var send = function(newMessage){
+          
+          
+          
+      };
+      
+      
+      
       return {          
-          getByRoomId : function(roomId){
-//              var queryAnswer = $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
-              var roomMessages = [];
-//              console.log(roomId);
-              if (roomId !== undefined){                  
-                  ref.orderByChild("roomId").equalTo(roomId).on('value', function(snapshot) {
-                      snapshot.val().forEach(function(element){
-//                          console.log("element: " + element);
-                          if (element != null){
-                              roomMessages.push(element);
-                          }
-                      });
-                  });                  
-              }
-              
-              return roomMessages;
-          }          
+          getByRoomId : getByRoomId,
+          send: send
       };
   }
 
